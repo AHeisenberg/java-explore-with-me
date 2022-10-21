a91417921a93d34a03cde8fc04f3e9eb66e7d031
Andreas <ageling@yandex.ru>
2022-10-21 21:01:33 +0200

    fix errors-----3

--- stats-service/src/main/java/ru/practicum/explore/controller/StatsController.java
+++ stats-service/src/main/java/ru/practicum/explore/controller/StatsController.java
@@ -1,5 +1,6 @@
 package ru.practicum.explore.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explore.dto.EndpointHit;
@@ -9,21 +10,11 @@ import ru.practicum.explore.service.StatsService;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * Контроллер статистики
- */
 @RestController
+@RequiredArgsConstructor
 public class StatsController {
-    private StatsService statsService;
+    private final StatsService statsService;
 
-    @Autowired
-    public StatsController(StatsService statsService) {
-        this.statsService = statsService;
-    }
-
-    /*
-    Метод контроллера по сохранению статистики
-    */
     @PostMapping("/hit")
     public EndpointHit save(@RequestBody EndpointHit endpointHit) {
         return statsService.save(endpointHit);
